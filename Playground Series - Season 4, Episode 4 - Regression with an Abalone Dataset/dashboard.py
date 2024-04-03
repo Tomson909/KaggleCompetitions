150f3d9e0e00bcbc91de84cc890aed2049348346
Tomson909 <tom.ruge@me.com>
2024-04-03 11:03:37 +0200

    Dashboard working now

--- Playground Series - Season 4, Episode 4 - Regression with an Abalone Dataset/dashboard.py
+++ Playground Series - Season 4, Episode 4 - Regression with an Abalone Dataset/dashboard.py
@@ -16,48 +16,23 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 # Load the dataset
 df_train = pd.read_csv('~/Schreibtisch/Data/Kaggle/playground-series-s4e4/train.csv')
 
-# Define categorical columns, binary columns are also categorical
-categorical_columns = df_train.select_dtypes(include=['object']).columns.tolist()
-# binary columns are also categorical, so when no unique of a column is 2, then it is a binary column
-categorical_columns.extend(df_train.columns[df_train.nunique() == 2].tolist())
+
+df_train.set_index('id', inplace=True)
 
 
-print('categorical:', categorical_columns)
+# Sex categorical
+categorical_columns = df_train.columns[df_train.nunique() == 3].tolist()
 # Define numerical columns
 numerical_columns = df_train.select_dtypes(include=['int64', 'float64']).columns.tolist()
 # exclude the binary columns, because they are categorial
 
-target_variable = 'Target'
-print('uniques:', df_train[target_variable].unique())
+target_variable = 'Rings'
 
 #---------------------------- Data preparation for correlatin ------------------------------
 # Splitting the dataset into features and target variable
 X = df_train.drop(target_variable, axis=1)
 y = df_train[target_variable]
 
-# Convert categorical features using one-hot encoding
-labelencoder = LabelEncoder()
-
-def mutual_information(df, target_variable):
-    df_deepcopy = deepcopy(df)
-    # make categorial columns from numerical columns with pandas cut
-    for col in df_deepcopy.columns:
-        if df_deepcopy[col].dtype == 'float64' or df_deepcopy[col].dtype == 'int64':
-            number_of_unique_values = len(df_deepcopy[col].unique())
-            column = pd.cut(df_deepcopy[col], 20)
-            df_deepcopy[col] = labelencoder.fit_transform(column)
-    # now label encode all categorial columns
-    for col in df_deepcopy.columns:
-        if df_deepcopy[col].dtype == 'object':
-            df_deepcopy[col] = labelencoder.fit_transform(df_deepcopy[col])
-    # now we can calculate the mutual information
-    mi = mutual_info_classif(df_deepcopy.drop(columns = target_variable), df_deepcopy[target_variable])
-    columns = df_deepcopy.drop(columns =target_variable).columns
-    # sort the columns by the mutual information
-    mi, columns = zip(*sorted(zip(mi, columns)))
-    return columns, mi, df_deepcopy
-
-columns, mi, df_deepcopy = mutual_information(df_train, target_variable)
 # -----------------------------End-------------------------------------
 
 # Define the layout of the dashboard
@@ -77,13 +52,6 @@ app.layout = dbc.Container([
         dcc.Markdown('### Missing values: Plot the missing values in the dataset with a bar chart'),
         dcc.Graph(figure=px.bar(df_train.isna().sum()), style={'height': '500px'})
     ]),
-    # target variable distribution with a pie chart
-    dbc.Row([
-        dcc.Markdown('### Pie chart: Distribution of the categorical variables'),
-        # select the target variable
-        dcc.Dropdown(id = 'pie_chart_target_variable', options = categorical_columns, value = categorical_columns[0]),
-        dcc.Graph(id = 'pie_chart', style = {'height': '500'})
-    ]),
     # Histogram for numerical columns
     dbc.Row([
         dcc.Markdown('### Histogram: Distribution of the numerical and categorial variables'),
@@ -98,7 +66,7 @@ app.layout = dbc.Container([
         dbc.Col([
             dcc.Dropdown(
                 id='histogram_color',
-                options=categorical_columns,
+                options= ['Rings', 'Sex'],
                 value=target_variable
             )], width=6),
 
@@ -126,29 +94,14 @@ app.layout = dbc.Container([
             dcc.Graph(id='2d_plot', style={'height': '800px'})
         ])
     ]),
+    # parallel coordinates
     dbc.Row([
-        dcc.Markdown('### Mutual Information'),
-        dcc.Markdown('The mutual information of a column with the target variable is a measure of how much information the column provides about the target variable. The higher the mutual information, the more important the column is for predicting the target variable. The mutual information is calculated using the mutual_info_classif function from scikit-learn. The numerical variables were converted to caterogorial variables with a histogram with 30 bins. Changing the number of bins also changes the entropys a little bit. But the overall trend remains.')
-    ]),
-    dbc.Row([
-        dcc.Graph(figure=px.bar(
-            x=columns,
-            y=mi,
-        ), style={'height': '800px'})
-
-    
+        dcc.Markdown('### Parallel Coordinates: Parallel coordinates plot of the numerical columns'),
+        dcc.Graph(figure=px.parallel_coordinates(df_train, dimensions= ['Length', 'Diameter', 'Height', 'Whole weight', 'Whole weight.1','Whole weight.2', 'Shell weight', 'Rings'], color='Rings'))
     ])
 ])
 
-# Callback to update the pie chart
-@app.callback(
-    Output('pie_chart', 'figure'),
-    Input('pie_chart_target_variable', 'value')
-)
-def update_pie_chart(target_variable):
-    print('uniwues_pie:', df_train[target_variable].unique())
-    fig = px.pie(df_train, names=target_variable, title=f'Distribution of {target_variable}', color=target_variable)
-    return fig
+
 
 # Callback to update the histogram
 @app.callback(
